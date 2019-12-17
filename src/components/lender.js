7e892e505aad5156f5af4a6ce70e23b8b2f16ff9
Rachel Aizenman <racheliamn@gmail.com>
2019-12-17 15:05:30 +0200

    added links

--- src/components/lender.js
+++ src/components/lender.js
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
 // import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { inject } from "mobx-react";
 
-
-export class Lender extends Component {
-    constructor() {
-        super()
-        this.state = {
-
-        }
-
-    }
+@inject('userStore')
+class Lender extends Component {
 
     render() {
-
+        const headArr = ['Username', 'Period (m)', 'Purpose', 'Amount', 'Interest', 'Remaining Amount', 'Status', 'Issuance Date', 'Next Payment' ]
+        const userStore = this.props.userStore
         return (
+            <Router>
             <div>
                 <div class='nav-bar'>
+                    <Link to={'/deposite'}>
                     <button>Deposite</button>
+                    </Link>
+                    <Link to={'/new-loan'}>
                     <button>Add Loan</button>
+                    </Link>
+                    <Link to={'/withdraws'}>
                     <button>Withdraw</button>
+                    </Link>
+                    <Link to={'/summary'}>
                     <button>Summary</button>
+                    </Link>
                     <div id='info-container'>
-                    <div id='protfolio-value' style={{borderRadius: '50%', border: '10px solid red', height: '150px', width: '150px', display: 'inline-block', margin: '10px'}}>Portfolio Value: </div>
-                    <div id='available-cash' style={{borderRadius: '50%', border: '10px solid red', height: '150px', width: '150px', display: 'inline-block', margin: '10px'}}>Available Cash: </div>
-                    <div id='add-investment' style={{borderRadius: '50%', border: '10px solid red', height: '150px', width: '150px', display: 'inline-block', margin: '10px'}}>Add Investment!</div>
-                    </div>
-                    <div class='open-loans' style={{ border: '3px solid black'}}> 
-                    <h3>OPEN LOANS: </h3>
-                    {/* <CustomizedTable head={'head'} data={'data'}/> */}
-                    <Link></Link>
+                        <div id='protfolio-value' style={{ borderRadius: '50%', border: '10px solid red', height: '150px', width: '150px', display: 'inline-block', margin: '10px' }}>Portfolio Value:  {userStore.user.length}$</div>
+                        <div id='available-cash' style={{ borderRadius: '50%', border: '10px solid red', height: '150px', width: '150px', display: 'inline-block', margin: '10px' }}>Available Cash: </div>
+                        </div>
+                        <Link to={'/market'}>
+                        <button id='add-investment' style={{ border: '10px solid green', height: '75px', display: 'inline-block', margin: '10px' }}>Add Investment!</button>
+                        </Link>
+                  
+                    <div class='open-loans' style={{ border: '3px solid black' }}>
+                        <h3>OPEN LOANS: </h3>
+                        {/* <CustomizedTable head={headArr} data={}/> */}
                         <table>
                             <tr>
                                 <th>Username</th>
@@ -69,6 +75,7 @@ export class Lender extends Component {
                     </div>
                 </div>
             </div>
+            </Router>
         )
     }
 }
