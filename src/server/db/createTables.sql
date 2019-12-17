56c845e845e048efc81b2289d419c0c1fd40baa5
Noam Lior <noamlior2@gmail.com>
2019-12-17 14:37:02 +0200

    bla

---
+++ src/server/db/createTables.sql
@@ -0,0 +1,29 @@
+-- create DATABASE p2p;
+USE p2p;
+create TABLE user(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30),
+    paswword VARCHAR(10),
+    type VARCHAR(1),
+    availableMoney INT
+);
+create TABLE loan(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    amount int,
+    interest int,
+    purpose varchar(20),
+    period int,
+    amountPaid int,
+    status varchar(20),
+    dateOfIssuance Date,
+    percentage int
+);
+create TABLE loan_lender(
+    loanID INT,
+    borrower INT,
+    lender INT,
+    percentage INT,
+    FOREIGN KEY (loanID) REFERENCES loan(id),
+    FOREIGN KEY (borrower) REFERENCES user(id),
+    FOREIGN KEY (lender) REFERENCES user(id)
+);
