3c7bb1a17cfd5c0b9edf1161f83c70741d9416a3
Rachel Aizenman <racheliamn@gmail.com>
2019-12-17 14:18:37 +0200

    first commit of lender

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { observer } from 'mobx-react'
+import Lender from './components/lender'
 
 
 @observer
@@ -9,23 +10,26 @@ class App extends Component {
   render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Lender/>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
   
   
