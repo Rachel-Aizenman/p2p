7db337b2695a699619b3e34ea066db1b93704f09
Rachel Aizenman <racheliamn@gmail.com>
2019-12-18 09:47:12 +0200

    slight route change

--- src/components/Lender/lenderNavBar.js
+++ src/components/Lender/lenderNavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { inject } from "mobx-react"
 
 @inject('userStore')
@@ -7,23 +7,22 @@ class navBar extends Component {
 
     render() {
         return (
-            <Router>
-            <div className='nav-bar'>
-            <Link to={'/deposit'}>
-            <button>Deposit</button>
-            </Link>
-            <Link to={'/new-loan'}>
-            <button>Add Loan</button>
-            </Link>
-            <Link to={'/withdraw'}>
-            <button>Withdraw</button>
-            </Link>
-            <Link to={'/summary'}>
-            <button>Summary</button>
-            </Link>
-            </div>  
-            </Router>     
-             )
+                <div className='nav-bar'>
+                    <Link to={'/deposit'}>
+                        <button>Deposit</button>
+                    </Link>
+                    <Link to={'/new-loan'}>
+                        <button>Add Loan</button>
+                    </Link>
+                    <Link to={'/withdraw'}>
+                        <button>Withdraw</button>
+                    </Link>
+                    <Link to={'/summary'}>
+                        <button>Summary</button>
+                    </Link>
+                </div>
+
+        )
     }
 }
 
--- src/components/Withdraw/withdraw.js
+++ src/components/Withdraw/withdraw.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { inject } from "mobx-react";
-import Slider from './Slider'
+import Slider from '../Slider'
 import axios from 'axios';
 
 @inject('userStore')
