33260b87a910dc68835cd1cf01fe0cf045a89e86
Rachel Aizenman <racheliamn@gmail.com>
2019-12-23 17:11:17 +0200

    on branch master

--- src/components/Withdraw/withdraw.js
+++ src/components/Withdraw/withdraw.js
@@ -5,8 +5,10 @@ import Slider from '../Slider'
 import axios from 'axios';
 import NavBar from '../navBar/NavBar'
 import moment from 'moment'
-@inject('UserStore', 'InputStore')
+import './Withdraw.css'
 
+
+@inject('UserStore', 'InputStore')
 @observer
 class Withdraw extends Component {
   withdraw = () => {
@@ -24,23 +26,18 @@ class Withdraw extends Component {
     return (
       <div>
         <NavBar />
-        <div id='available-cash' style={{ borderRadius: '50%', border: '10px solid red', height: '150px', width: '150px', display: 'inline-block', margin: '10px' }}>
+        <div id='available-cash'>
           Portfolio Value:${this.props.UserStore.user.totalWorth}
         </div>
 
-        <div id='available-cash' style={{ borderRadius: '50%', border: '10px solid red', height: '150px', width: '150px', display: 'inline-block', margin: '10px' }}>
+        <div id='available-cash'>
           Available Cash: ${this.props.UserStore.user.availableCash}
         </div>
-
-
-        <div className='explanation' style={{ width: '100vw', backgroundColor: 'grey', position: 'inline-block', padding: '15px' }}>
-          The poem "Jabberwocky" in Lewis Carroll's Through the Looking Glass is perhaps the most famous example of gibberish. Lewis Carroll, whose real name was actually Charles Lutwidge Dodgson, was famed for his love of nonsensical language and inventing new words. Here is an extract from the poem:ddition to nonsense words, phrases and sentences, there is also a language called Gibberish. The language is similar to Pig Latin and is used by people who want to play games with a secret language.
-          To speak the language, you break each word down into its syllables. Each syllable will usually have a vowel sound. You then add otha-g before each vowel sound. Some examples of Gibberish words (and their English translations) include:
-            </div>
         <div id='slider'>
+          
           <Slider name='withdrawInput' handleInput={this.props.InputStore.handleInput} />
         </div>
-        <button onClick={this.withdraw}>Withdraw</button>
+        <button className='btn'onClick={this.withdraw}>Withdraw</button>
 
 
       </div>
